c5d020e1afa48b697fbca0afbef20078fa595372
ah <ah>
2013-10-08 15:44:21 +0200

    moved linear relation label more to the center

--- macros/FedSizePerXUtils.py
+++ macros/FedSizePerXUtils.py
@@ -372,7 +372,7 @@ class Plotter:
             self.fittedFunc.SetLineWidth(3)
             self.fittedFunc.SetLineColor(ROOT.kBlue)
 
-            label = ROOT.TLatex(0.25,0.9,self.fitResultLabel)
+            label = ROOT.TLatex(0.20,0.9,self.fitResultLabel)
             label.SetNDC(1)
             label.Draw()
 
