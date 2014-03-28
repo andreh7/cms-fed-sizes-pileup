c1cf160ce8641c80ca0b82ba2ff4e9a2a99217b1
ah <ah>
2014-03-28 15:39:13 +0100

    added support for run 179827

--- macros/setup.sh
+++ macros/setup.sh
@@ -26,6 +26,11 @@ fi
 
 if [ $RUN = 198588 ] || [ $RUN = 198609 ]; then
   CMSSW_VERSION=5_3_2_patch2 
+elif [ $RUN = 179827 ] ; then
+
+  export SCRAM_ARCH=slc5_amd64_gcc451
+  CMSSW_VERSION=4_4_0_patch3
+
 else
   echo "don't know which CMSSW version to use for run $RUN"
   return
