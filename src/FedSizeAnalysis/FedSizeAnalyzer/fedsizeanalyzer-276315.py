d2abee9d855f055a53b520420be690f008e25804
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-07-10 23:10:26 +0200

    added files for run 276315

---
+++ src/FedSizeAnalysis/FedSizeAnalyzer/fedsizeanalyzer-276315.py
@@ -0,0 +1,28 @@
+
+# good_lumi_section_json_file is the list of 'good lumisections' to be
+# used for filtering.
+#
+# set this to None if you don't want to apply any filtering
+# (e.g. when running on the express stream)
+#--------------------
+
+# see https://hypernews.cern.ch/HyperNews/CMS/get/physics-validation/2648/1/1.html
+#good_lumi_section_json_file = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273450_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt"
+
+# DCS only for the moment (2016-07-10)
+good_lumi_section_json_file = "json_DCSONLY.txt"
+
+#--------------------
+
+# set this to a positive number if you want
+# to restrict running to less than the maximum
+# number of events (useful for testing)
+maxEvents = -1
+
+# set this here to keep additional products
+keepProducts = []
+
+keepL1MenuInformation = True
+
+
+# useRECO = False
