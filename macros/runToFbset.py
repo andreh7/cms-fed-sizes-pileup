903c366daa464e24a345d283caac279bed6ad8b5
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-08-02 17:29:11 +0200

    added fbset for run 273301

--- macros/runToFbset.py
+++ macros/runToFbset.py
@@ -5,6 +5,9 @@ runToFbset = {
     273158 : "fb_all_withuTCA_consolidated3_no1240_TOTEM",
 
     275832 : "fb_all_withuTCA_with_CTPPS_TOT",
+
+    273301 : "fb_all_withuTCA_consolidated3_no1240_TOTEM",
+
     }
 
 def getFbsetFromRun(run):
