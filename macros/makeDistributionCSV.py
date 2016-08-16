425ee4740117e7ea8a525e3168fd7a63848cbc8f
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-08-16 12:50:20 +0200

    initial commit: tool to write out a .csv file with the quantiles vs. number of reconstructed vertex for the FED groups looked at

---
+++ macros/makeDistributionCSV.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# given a plot output directory, produces a CSV file
+# with the quantiles of the distributions per subsystem analyzed
+
+import sys, os
+
+#----------------------------------------------------------------------
+# main
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+
+
+    ARGV = sys.argv[1:]
+
+    assert len(ARGV) == 1
+
+    tasksFile = ARGV.pop(0)
+
+    import pickle
+
+    tasks = pickle.load(open(tasksFile))
+
+    plotDir = os.path.dirname(tasksFile)
+    outputFname = os.path.join(plotDir, "distributions.csv")
+
+    fout = open(outputFname, "w")
+
+    #----------
+    from FedSizePerVertexLinearFit import FedSizePerVertexLinearFit
+
+    isFirst = True
+    for task in tasks:
+        if not isinstance(task, FedSizePerVertexLinearFit):
+            continue
+
+        task.writeQuantilesCSV(fout, isFirst)
+        isFirst = False
+
+    fout.close()
+    print >> sys.stderr,"wrote report to", outputFname
+
