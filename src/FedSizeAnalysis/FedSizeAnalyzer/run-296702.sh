a1154ec9f1b85825cb357de1eb26b75188035240
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-07-04 13:12:03 +0200

    changed dataset in run-296702.sh

--- src/FedSizeAnalysis/FedSizeAnalyzer/run-296702.sh
+++ src/FedSizeAnalysis/FedSizeAnalyzer/run-296702.sh
@@ -8,6 +8,6 @@ for startLS in $(seq $FIRST_LS $LS_PER_JOB $LAST_LS) ; do
 
   lastLS=$[$startLS + $LS_PER_JOB - 1]
 
-  cmsRun fedsizeanalyzer_cfg.py $startLS $lastLS hltphysics $RUN > log-$RUN-$startLS-$lastLS.log 2>&1 &
+  cmsRun fedsizeanalyzer_cfg.py $startLS $lastLS zerobias $RUN > log-$RUN-$startLS-$lastLS.log 2>&1 &
 
 done
