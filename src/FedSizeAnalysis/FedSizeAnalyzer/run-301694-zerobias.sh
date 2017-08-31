2f868ee0db7db16bb1894fa0b11e49adf0709a93
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-08-31 12:39:22 +0200

    added file list and run.sh for run 301694 zero bias

---
+++ src/FedSizeAnalysis/FedSizeAnalyzer/run-301694-zerobias.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FIRST_LS=36
+LAST_LS=102
+LS_PER_JOB=100
+
+# need export to see it from subprocesses 
+export RUN=301694
+
+for startLS in $(seq $FIRST_LS $LS_PER_JOB $LAST_LS) ; do
+
+  lastLS=$[$startLS + $LS_PER_JOB - 1]
+
+  cmsRun fedsizeanalyzer_cfg.py $startLS $lastLS zerobias $RUN > log-$RUN-zerobias-$startLS-$lastLS.log 2>&1 &
+
+done
