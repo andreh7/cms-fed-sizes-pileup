634a04915505d4cf43932de99aa0bf4f1f90fc5d
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-09-04 16:09:31 +0200

    added file list and run.sh for high pileup bunch crossing datasets

---
+++ src/FedSizeAnalysis/FedSizeAnalyzer/run-283171-hltphysicsHighpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./run.py \
+  --run 283171 \
+  --json /home/users/aholz/2016-05-fed-size-analysis/CMSSW_8_0_23/src/FedSizeAnalysis/FedSizeAnalyzer/json-283171.txt \
+  --dstitle hltphysicsHighpu \
+  --njobs 30
+
