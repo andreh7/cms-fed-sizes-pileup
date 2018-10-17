4acd59bd6aa5776ede79dfb3c2a54b0c84043d88
Andre Holzner <andre.holzner@gmail.com>
2018-10-17 12:43:24 +0200

    added files for run 302674

---
+++ src/FedSizeAnalysis/FedSizeAnalyzer/run-302674-zerobiasIsolated.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./run.py \
+  --run 302674 \
+  --json /home/users/aholz/2016-05-fed-size-analysis/CMSSW_9_2_10/src/FedSizeAnalysis/FedSizeAnalyzer/json_DCSONLY-302674.txt \
+  --dstitle zerobiasIsolated \
+  --njobs 30
+
